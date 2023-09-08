da01809cbdd6926c768f40482773dc5eea204156
Marckus Santiago <marckus.santiago@pcworth.com>
2023-09-08 09:41:20 +0800

    feat: login page

--- app/src/main/java/com/example/trackit/MainActivity.kt
+++ app/src/main/java/com/example/trackit/MainActivity.kt
@@ -16,31 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TrackitTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("tite")
-                }
-            }
+            setContentView(R.layout.activity_actual_main)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrackitTheme {
-        Greeting("World")
     }
 }
