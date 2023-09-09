827f3ef7b9e09a3e343d5f9342db8fdf52c0f05b
Marckus Santiago <marckus.santiago@pcworth.com>
2023-09-09 22:19:00 +0800

    feat: made signup ui

--- app/src/main/java/com/example/trackit/MainActivity.kt
+++ app/src/main/java/com/example/trackit/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            //TODO: link ui
             setContentView(R.layout.activity_actual_main)
         }
     }
