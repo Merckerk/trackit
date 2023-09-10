86f7d6a530651553fdaf67b0cbbb6714801a483f
Marckus Santiago <marckus.santiago@pcworth.com>
2023-09-10 16:49:35 +0800

    fix: changed java files to kt files.

---
+++ app/src/main/java/com/example/trackit/progressMenuActivity.kt
@@ -0,0 +1,11 @@
+package com.example.trackit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class progressMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_progress_menu)
+    }
+}
